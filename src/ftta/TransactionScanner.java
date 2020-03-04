fc2f9930ad5e6521bf4caf23fdd25d9246e16dde
OwenPiercey <me@owenpiercey.ca>
2020-03-04 16:14:10 -0400

    Finished TransactionScanner ~ and updated Transaction class

--- src/ftta/TransactionScanner.java
+++ src/ftta/TransactionScanner.java
@@ -7,35 +7,27 @@ import java.io.IOException;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.FormulaEvaluator;
 import org.apache.poi.ss.usermodel.Row;
 
 public class TransactionScanner {
-	public static void main(String args[]) throws IOException {
-		// obtaining input bytes from a file
-		FileInputStream fis = new FileInputStream(new File("/Users/owenpiercey/Desktop/student.xls"));
-		// creating workbook instance that refers to .xls file
+	
+	public void ScanExcelSheet() throws IOException {
+		FileInputStream fis = new FileInputStream(new File("/team6-ftta/TestFiles/SampleBankRecord.xls"));
 		HSSFWorkbook wb = new HSSFWorkbook(fis);
-		// creating a Sheet object to retrieve the object
 		HSSFSheet sheet = wb.getSheetAt(0);
-		// evaluating cell type
-		FormulaEvaluator formulaEvaluator = wb.getCreationHelper().createFormulaEvaluator();
-		for (Row row : sheet) // iteration over row using for each loop
-		{
-			for (Cell cell : row) // iteration over cell using for each loop
-			{
-				switch (formulaEvaluator.evaluateInCell(cell).getCellType()) {
-				case Cell.CELL_TYPE_NUMERIC: // field that represents numeric cell type
-					// getting the value of the cell as a number
-					System.out.print(cell.getNumericCellValue() + "\t\t");
-					break;
-				case Cell.CELL_TYPE_STRING: // field that represents string cell type
-					// getting the value of the cell as a string
-					System.out.print(cell.getStringCellValue() + "\t\t");
-					break;
-				}
+		
+		TransactionList tList = new TransactionList();
+		
+		for (int i = 1; i < sheet.getLastRowNum()+1; i++) {
+			Row row = sheet.getRow(i);
+			
+			if (row.getCell(3) == null || row.getCell(3).getCellType() == Cell.CELL_TYPE_BLANK) {
+				Transaction transaction = new Transaction(row.getCell(0).getStringCellValue(), row.getCell(1).getStringCellValue(), row.getCell(2).getNumericCellValue() , 0);
+				tList.addToTransactions(transaction);
+			}else if(row.getCell(2) == null || row.getCell(2).getCellType() == Cell.CELL_TYPE_BLANK) {
+				Transaction transaction = new Transaction(row.getCell(0).getStringCellValue(), row.getCell(1).getStringCellValue(), 0, row.getCell(3).getNumericCellValue());
+				tList.addToTransactions(transaction);
 			}
-			System.out.println();
 		}
 	}
 }
