a177e8b4622e909c5d97498d4c066af768bb018f
Gabriel Ibarra <gibarralopez98@gmail.com>
2020-03-04 17:57:21 -0400

    barebones excel file writer

---
+++ src/ftta/FileWriter.java
@@ -0,0 +1,57 @@
+package ftta;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+ 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+ 
+/**
+ * A very simple program that writes some data to an Excel file
+ * using the Apache POI library.
+ * @author www.codejava.net
+ *
+ */
+public class FileWriter {
+ 
+    public static void main(String[] args) throws IOException {
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet = workbook.createSheet("Transaction Record");
+         
+        Object[][] bookData = {
+                {"Transaction", "Credit", "Debit"},
+                {"Effective Java", "Joshua Bloch", 36},
+                {"Clean Code", "Robert martin", 42},
+                {"Thinking in Java", "Bruce Eckel", 35},
+        };
+ 
+        int rowCount = 0;
+         
+        for (Object[] aBook : bookData) {
+        	
+            Row row = sheet.createRow(rowCount);
+            rowCount++;
+             
+            int columnCount = 0;
+             
+            for (Object field : aBook) {
+                Cell cell = row.createCell(columnCount);
+                columnCount++;
+                if (field instanceof String) {
+                    cell.setCellValue((String) field);
+                } else if (field instanceof Integer) {
+                    cell.setCellValue((Integer) field);
+                }
+            }
+             
+        }
+         
+         
+        try (FileOutputStream outputStream = new FileOutputStream("TestFiles/TransactionReport.xlsx")) {
+            workbook.write(outputStream);
+        }
+    }
+ 
+}
