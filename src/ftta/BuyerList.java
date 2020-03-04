70eddb0bc68494112e02a781eb0564462874d301
PierceyOwen <59845614+PierceyOwen@users.noreply.github.com>
2020-03-04 19:00:17 -0400

    Update BuyerList.java

--- src/ftta/BuyerList.java
+++ src/ftta/BuyerList.java
@@ -6,15 +6,15 @@ public class BuyerList {
 	ArrayList<Buyer> buyers = new ArrayList<Buyer>();
 
     public void addBuyerToList(Buyer buyer) {
-    	boolean flag = false;
+    	boolean buyerExists = false;
     	
         for (int i = 0; i < buyers.size(); i++) {
         	if (buyers.get(i).getID() == buyer.getID()) {
-        		flag = true;
+        		buyerExists = true;
         	}
         }
         
-        if (!flag) {
+        if (!buyerExists) {
     		buyers.add(buyer);
         }
     }
